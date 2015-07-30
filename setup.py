1897d91e96ec782c335047a7f532693167035b77
Stefan Seemayer <stefan@seemayer.de>
2015-07-30 15:23:05 +0200

    Add MIT license

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 
-setup(	name='qstat-pretty',
-	version='0.0.1',
-	author='Stefan Seemayer',
-	author_email='mail@semicolonsoftware.de',
-	url='https://github.com/sseemayer/qstat-pretty',
+setup(
+    name='qstat-pretty',
+    version='0.0.1',
+    author='Stefan Seemayer',
+    author_email='stefan@seemayer.de',
+    license='MIT',
+    url='https://github.com/sseemayer/qstat-pretty',
 
-	packages=['qstatpretty', 'qstatpretty.ttyutil'],
-	scripts=['pstat']
+    packages=['qstatpretty', 'qstatpretty.ttyutil'],
+    scripts=['pstat']
 )
